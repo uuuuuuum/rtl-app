1967d34fec375d46b61650c4c76a7f724f428d0a
youme git <youmechoi9@gmail.com>
2022-09-14 20:13:44 +0900

    Add : counter test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  /** @explain screen object를 이용해서 원하는 element에 data-testid로 접근 */
+  const counterElement = screen.getByTestId("counter");
+
+  /** @explain data-testid가 counter인 element의 텍스트가 0인지 테스트 */
+  expect(counterElement).toHaveTextContent(0);
 });
