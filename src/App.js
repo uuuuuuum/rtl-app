fafc66efab04c3428508178f75ea9ae34f3d0b74
youme git <youmechoi9@gmail.com>
2022-09-14 20:30:19 +0900

    add : plus minus buttons feature use fireEvent.click

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button
+            data-testid="minus-button"
+            onClick={() => setCounter((prev) => prev - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter((prev) => prev + 1)}
+          >
+            +
+          </button>
         </div>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counter starts at 0", () => {
@@ -29,4 +29,32 @@ test("plus button has correct test", () => {
 
   /** @explain data-testid가 minus-button인 element의 텍스트가 +인지 테스트 */
   expect(plusButtonElement).toHaveTextContent("+");
+});
+
+// 플러스 버튼 클릭시 counter 0 > 1로 변경
+test("when the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("plus-button");
+
+  // click plus button
+  fireEvent.click(buttonElement);
+
+  // 카운터가 0에서 1이 됩니다.
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+// 마이너스 버튼 클릭시 counter 0 > -1로 변경
+test("when the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+
+  // click plus button
+  fireEvent.click(buttonElement);
+
+  // 카운터가 0에서 -1이 됩니다.
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
 });
