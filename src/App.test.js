9d0385c524c3083d8b3b851e238285fb049bcf38
youme git <youmechoi9@gmail.com>
2022-09-14 20:51:14 +0900

    code cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -13,31 +13,22 @@ test("the counter starts at 0", () => {
 
 test("minus button has correct test", () => {
   render(<App />);
-
-  /** @explain screen object를 이용해서 원하는 element에 data-testid로 접근 */
   const minusButtonElement = screen.getByTestId("minus-button");
-
-  /** @explain data-testid가 minus-button인 element의 텍스트가 -인지 테스트 */
   expect(minusButtonElement).toHaveTextContent("-");
 });
 
 test("plus button has correct test", () => {
   render(<App />);
-
-  /** @explain screen object를 이용해서 원하는 element에 data-testid로 접근 */
   const plusButtonElement = screen.getByTestId("plus-button");
-
-  /** @explain data-testid가 minus-button인 element의 텍스트가 +인지 테스트 */
   expect(plusButtonElement).toHaveTextContent("+");
 });
 
-// 플러스 버튼 클릭시 counter 0 > 1로 변경
+// + 버튼 클릭시 counter 0 > 1로 변경
 test("when the + button is pressed, the counter changes to 1", () => {
   render(<App />);
-
   const buttonElement = screen.getByTestId("plus-button");
 
-  // click plus button
+  // click + button
   fireEvent.click(buttonElement);
 
   // 카운터가 0에서 1이 됩니다.
@@ -45,13 +36,12 @@ test("when the + button is pressed, the counter changes to 1", () => {
   expect(counterElement).toHaveTextContent(1);
 });
 
-// 마이너스 버튼 클릭시 counter 0 > -1로 변경
+// - 버튼 클릭시 counter 0 > -1로 변경
 test("when the - button is pressed, the counter changes to -1", () => {
   render(<App />);
-
   const buttonElement = screen.getByTestId("minus-button");
 
-  // click plus button
+  // click - button
   fireEvent.click(buttonElement);
 
   // 카운터가 0에서 -1이 됩니다.
@@ -67,7 +57,7 @@ test("on/off button has blue color", () => {
   expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
 });
 
-// on/off 버튼 클릭시 - + 플러스 버튼 disalbed가 true로 바뀌는지 확인
+// on/off 버튼 클릭시 - + 버튼 disalbed가 true로 바뀌는지 확인
 test("Prevent the - + button from being pressed when the on/off button is clicked", () => {
   render(<App />);
 
