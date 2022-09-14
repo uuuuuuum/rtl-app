5613fb4655dcf9e78e3c1b67b532a9c7de79730a
youme git <youmechoi9@gmail.com>
2022-09-14 20:19:15 +0900

    Add : test that minus and plus buttons have correct text

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,23 @@ test("the counter starts at 0", () => {
   /** @explain data-testid가 counter인 element의 텍스트가 0인지 테스트 */
   expect(counterElement).toHaveTextContent(0);
 });
+
+test("minus button has correct test", () => {
+  render(<App />);
+
+  /** @explain screen object를 이용해서 원하는 element에 data-testid로 접근 */
+  const minusButtonElement = screen.getByTestId("minus-button");
+
+  /** @explain data-testid가 minus-button인 element의 텍스트가 -인지 테스트 */
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct test", () => {
+  render(<App />);
+
+  /** @explain screen object를 이용해서 원하는 element에 data-testid로 접근 */
+  const plusButtonElement = screen.getByTestId("plus-button");
+
+  /** @explain data-testid가 minus-button인 element의 텍스트가 +인지 테스트 */
+  expect(plusButtonElement).toHaveTextContent("+");
+});
